9e1d5d95b887a997a17b694146f66b9a2ceea028
Manoel Junior <manoelluisjunior@hotmail.com>
2021-02-18 15:31:03 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/manoeljunior/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.manoeljunior.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manoeljunior.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> { //Herda uma outra interface, que é capaz de acessar os dados com base deum tipo que vc passar//
+	
+}
